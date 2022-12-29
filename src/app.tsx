f2fbcafd0651826980ef3c9133b409a831dc5ba1
alexperronnet <github.ayfb9@lxprnt.com>
2022-12-29 01:52:20 +0100

    Init the router

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,26 @@
 import { FC } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { theme, GlobalStyle } from '@/styles'
+import { Home, About, Accommodation, NotFound } from '@/pages'
+import { useFetchData } from '@/hooks'
 
 export const App: FC = () => {
+  const { data } = useFetchData()
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/a-propos" element={<About />} />
+          {data?.map(item => (
+            <Route key={item.id} path={`/hebergement/${item.id}`} element={<Accommodation />} />
+          ))}
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { Home } from '@/pages/home'
+export { About } from '@/pages/about'
+export { Accommodation } from '@/pages/accommodation'
+export { NotFound } from '@/pages/not-found'
